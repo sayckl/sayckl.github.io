54a7a8d6d1d95b9dab4c3e4bf8de2388076e0cc3
sayckl <34318594+sayckl@users.noreply.github.com>
2019-06-07 20:02:59 +0300

    Add files via upload

---
+++ test-page/comments.js
@@ -0,0 +1,15 @@
+import addcomment from 'script.js';
+new Vue({
+  el: '#comments-list',
+  data: {
+    return: {
+    comments: [],
+  },
+  },
+  mounted() {
+    fetch('https://5cbef81d06a6810014c66193.mockapi.io/api/comments/')
+    .then(response => response.json())
+    .then(data => comments = data);
+  },
+
+})
---
+++ test-page/script.js
@@ -0,0 +1,18 @@
+export default addcomment
+new Vue({
+  el: '#writecom',
+  data: {
+    titlevalue: '',
+    bodyvalue: '',
+
+  },
+  methods: {
+    addcomment: function() {
+      if(this.titlevalue !== '' || this.bodyvalue !== '') {
+        this.comments.push({"id": comments[comments.length - 1].id + 1, create_at": new Date().getTime(), "title":this.titlevalue, "body": this.bodyvalue });
+        this.titlevalue = '';
+        this.bodyvalue = '';
+      },
+    },
+  },
+})
