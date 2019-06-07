b52497221f726a1185722b56bbed1081de8c3e63
sayckl <34318594+sayckl@users.noreply.github.com>
2019-06-07 21:34:18 +0300

    Update comments.js

--- test-page/comments.js
+++ test-page/comments.js
@@ -2,7 +2,9 @@
 new Vue({
   el: '#comments-list',
   data() {
-    return {};
+    return {
+      comments: [],
+    };
   },
   mounted() {
     fetch('https://5cbef81d06a6810014c66193.mockapi.io/api/comments/')
