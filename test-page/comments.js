27cf50b99f88d22cfc47d9e7a73bc928466fe27e
sayckl <34318594+sayckl@users.noreply.github.com>
2019-06-09 21:26:29 +0300

    Add files via upload

--- test-page/comments.js
+++ test-page/comments.js
@@ -10,5 +10,8 @@ new Vue({
     fetch('https://5cbef81d06a6810014c66193.mockapi.io/api/comments/')
     .then(response => response.json())
     .then(data => this.comments = data)
+    .catch(() => {
+      alert('пездес');
+    })
   },
 })
